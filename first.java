1b654f64dfeee4a0d0a013d52f1bdbfbefb6aec2
prabashmalhara <e21328@eng.pdn.ac.lk>
2025-07-09 14:08:50 +0530

    add first java file to repo

--- first.java
+++ first.java
@@ -1,1 +1,10 @@
-
+import java.util.*;
+public class first {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        System.out.println("You entered: " + number);
+        scanner.close();
+    }
+}
